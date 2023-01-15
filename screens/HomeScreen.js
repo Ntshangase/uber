d5753f77d935c3f50160ab96ece48eba48a48b74
Ntshangase <simphiwe323i@gmail.com>
2023-01-15 22:37:53 +0200

    feat: homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,25 @@
-import { SafeAreaView, StyleSheet, Text } from 'react-native'
-import React from 'react'
+import { Image, SafeAreaView, StyleSheet, View } from 'react-native'
+import React from 'react';
+import tw from 'twrnc';
+import NavOptions from '../components/NavOptions';
 
 const HomeScreen = () => {
-  return (
-    <SafeAreaView>
-        <Text style={styles.text}>This is the HomeScreen</Text>
-    </SafeAreaView>
-  )
+    return (
+        <SafeAreaView style={tw`bg-white h-full`}>
+            <View style={tw`p-5`}>
+                <Image
+                    style={{
+                        width: 100, height: 200, resizeMode: 'contain'
+                    }}
+                    source={{
+                        uri: 'https://links.papareact.com/gzs',
+                    }}
+                />
+
+                <NavOptions />
+            </View>
+        </SafeAreaView>
+    )
 }
 
 export default HomeScreen
