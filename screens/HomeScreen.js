9ff6cecf4fadcf1791b043b6457744e601dc38d1
Ntshangase <simphiwe323i@gmail.com>
2023-01-16 15:59:31 +0200

    feat: setLocation

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,9 +3,13 @@ import React from 'react';
 import tw from 'twrnc';
 import NavOptions from '../components/NavOptions';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import { GOOGLE_MAPS_APIKEY} from '@env';
+import { GOOGLE_MAPS_APIKEY } from '@env';
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from '../slices/navSlice';
 
 const HomeScreen = () => {
+    const dispatch = useDispatch();
+
     return (
         <SafeAreaView style={tw`bg-white h-full`}>
             <View style={tw`p-5`}>
@@ -18,8 +22,32 @@ const HomeScreen = () => {
                     }}
                 />
 
-                <GooglePlacesAutocomplete 
+                <GooglePlacesAutocomplete
                     placeholder='Where From?'
+                    styles={{
+                        container: {
+                            flex: 0,
+                        },
+                        textInput: {
+                            fontSize: 10,
+                        },
+                    }}
+                    onPress={(data, details = null) => {
+                        dispatch(setOrigin({
+                            location: details.geometry.location,
+                            description: data.description
+                        })
+                        );
+                        dispatch(setDestination(null));
+                    }}
+                    fetchDetails={true}
+                    returnKeyType={'search'}
+                    enablePoweredByContainer={false}
+                    minLength={2}
+                    query={{
+                        key: GOOGLE_MAPS_APIKEY,
+                        language: 'en'
+                    }}
                     nearbyPlacesAPI='GooglePlacesSearch'
                     debounce={400}
                 />
