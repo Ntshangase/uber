35a03b787f94c74b19722eb010f521e5031470fd
Ntshangase <simphiwe323i@gmail.com>
2023-01-18 18:27:55 +0200

    learned something

--- components/Map.js
+++ components/Map.js
@@ -1,11 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import MapView from 'react-native-maps';
+import tw from 'twrnc';
+import { useSelector } from 'react-redux';
+import { selectOrigin } from '../slices/navSlice';
 
 const Map = () => {
+  const origin = useSelector(selectOrigin);
+
+
   return (
-    <View>
-      <Text> I'm the Map</Text>
-    </View>
+    <MapView
+      style={tw`flex-1`}
+      mapType='mutedStandard'
+      initialRegion={{
+        latitude: origin.location.lat,
+        longitude: origin.location.lng,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      }}
+    />
   )
 }
 
