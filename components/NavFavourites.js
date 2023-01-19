be638f34758146abf2c7a79f5d242b72133306f1
Ntshangase <simphiwe323i@gmail.com>
2023-01-19 03:05:44 +0200

    feat: x

--- components/NavFavourites.js
+++ components/NavFavourites.js
@@ -1,18 +1,20 @@
 import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { Icon } from '@rneui/base';
+import tw from 'twrnc';
 
 const data = [
     {
         id: '123',
         icon: 'home',
         location: 'home',
-        destination: 'Code Street, London, UK',
+        destination: 'Anton Lembede, Durban, ZA',
     },
     {
         id: '456',
         icon: 'briefcase',
         location: 'Work',
-        destination: 'London Eye, Londo, UK',
+        destination: 'Umhlanga Drive, Durban, ZA',
     },
 ];
 
@@ -22,9 +24,22 @@ const NavFavourites = () => {
             <FlatList
                 data={data}
                 keyExtractor={(item) => item.id}
-                renderItem={({ item }) => (
-                    <TouchableOpacity>
-                        <Text>Sick Ngampela..</Text>
+                ItemSeparatorComponent={ () => (
+                    <View  style={ [tw`bg-gray-200`, {height: 0.5}]} />
+                )}
+                renderItem={({ item: {location, destination, icon} }) => (
+                    <TouchableOpacity style={tw`flex-row items-center p-3`}>
+                        <Icon
+                            style={tw`mr-4 rounded-full bg-gray-300 p-3`}
+                            name={icon}
+                            type='ionicon'
+                            color='white'
+                            size={18}
+                        />
+                        <View>
+                            <Text style={tw`font-semibold text-lg`}>{location}</Text>
+                            <Text style={tw`text-gray-500`}>{destination}</Text>
+                        </View>
                     </TouchableOpacity>
                 )}
             />
