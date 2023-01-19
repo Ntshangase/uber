4acc447567e72f33b10e0f64dbd71c5a8e855182
Ntshangase <simphiwe323i@gmail.com>
2023-01-19 16:17:58 +0200

    feat: uber cars icons

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,8 +1,29 @@
-import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native'
+import { FlatList, SafeAreaView, Text, TouchableOpacity, View, Image } from 'react-native'
 import React from 'react';
 import tw from 'twrnc';
 import { Icon } from '@rneui/base';
 import { useNavigation } from '@react-navigation/native';
+
+const data = [
+  {
+    id: 'Uber-X-123',
+    title: 'UberX',
+    multiplier: 1,
+    image: 'https://links.papareact.com/3pn',
+  },
+  {
+    id: 'Uber-XL-456',
+    title: 'Uber XL',
+    multiplier: 1.2,
+    image: 'https://links.papareact.com/5w8',
+  },
+  {
+    id: 'Uber-LUX-789',
+    title: 'Uber LUX',
+    multiplier: 1.75,
+    image: 'https://links.papareact.com/7pf',
+  },
+];
 
 const RideOptionsCard = () => {
   const navigation = useNavigation();
@@ -17,6 +38,28 @@ const RideOptionsCard = () => {
         </TouchableOpacity>
         <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
       </View>
+
+      <FlatList 
+        data={data}
+        keyExtractor={ (item) => item.id}
+        renderItem={ ({item:{id, title, multiplier, image}}) => (
+          <TouchableOpacity style={tw`flex-row justify-between items-center px-10`}>
+            <Image 
+              style={ {
+                width: 100,
+                height: 100,
+                resizeMode: 'contain',
+              }} 
+              source={ {uri: image}}
+            />
+            <View>
+              <Text>{title}</Text>
+              <Text>Travel time...</Text>
+            </View>
+            <Text>R99</Text>
+          </TouchableOpacity>
+        )}
+      />
     </SafeAreaView>
   )
 }
